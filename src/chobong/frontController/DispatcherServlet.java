c4949d296afc292b7753dc92f16b4c706c7edf3f
kosta <kosta@192.168.0.105>
2019-07-08 21:24:39 +0900

    맵핑명만 바꿈

--- src/chobong/frontController/DispatcherServlet.java
+++ src/chobong/frontController/DispatcherServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/elec")
+@WebServlet("/movie")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Map<String, Controller> map;
